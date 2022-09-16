593ad38470e5191825c01c693d8cdbbd0539787b
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-09-15 22:07:32 -0500

    Added 'loadTasks' & 'saveTasks' variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,24 @@
+
+// Defining initial variables
 var currentDayEl = document.querySelector("#currentDay");
 var timeEl = document.querySelector(".timeblock .time").innerText;
 console.log(timeEl);
 
+// Defining Date and Time variables
 var nowDate = moment().format("MMMM Do YYYY");
 var nowTime = moment().format("hh:mm:ss A");
 var compareTime = moment().format("hA");
 
+// logging Date and Time Parameters 
 console.log(moment().hour());
 console.log(nowDate);
 console.log(nowTime);
 console.log(compareTime);
+
+// loading input texts into Storage 
+var loadTasks = function() {
+  task = JSON.parse(localStorage.getItem("text"));
+}
 
 var setCurrentDay = function () {
   
@@ -17,10 +26,15 @@ var setCurrentDay = function () {
   return setDate;
 };
 
+// saving input texts into Storage
 var saveTasks = function() {
-  localStorage.setItem("text", JSON.stringify(innerText));
+  localStorage.setItem("text", JSON.stringify(text));
 };
 
 var convertTime = function (value) {};
 
+saveTasks();
+
 setCurrentDay();
+
+
