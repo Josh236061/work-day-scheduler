91581261747b7da0f4134b37caf03d59b0d7e8ec
Josh M Thomas <71394743+Josh236061@users.noreply.github.com>
2022-09-22 14:45:51 -0500

    UPDATED 'js script' file w/ new color-coded 'past, present, future' color declaration functions; fixed localStorage reloading 'saved data-present' issues

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,40 +1,67 @@
 
-// Defining initial variables
-var currentDayEl = document.querySelector("#currentDay");
-var timeEl = document.querySelector(".timeblock .time").innerText;
-console.log(timeEl);
+/**
+ * DEFINE VARIABLES
+ */
 
-// Defining Date and Time variables
-var nowDate = moment().format("MMMM Do YYYY");
-var nowTime = moment().format("hh:mm:ss A");
-var compareTime = moment().format("hA");
+ var saveBtn = $(".saveBtn");
 
-// logging Date and Time Parameters 
-console.log(moment().hour());
-console.log(nowDate);
-console.log(nowTime);
-console.log(compareTime);
-
-// loading input texts into Storage 
-var loadTasks = function() {
-  task = JSON.parse(localStorage.getItem("text"));
-}
-
-var setCurrentDay = function () {
-  
-  var setDate = (currentDayEl.innerText = nowDate);
-  return setDate;
-};
-
-// saving input texts into Storage
-var saveTasks = function() {
-  localStorage.setItem("text", JSON.stringify(text));
-};
-
-var convertTime = function (value) {};
-
-saveTasks();
-
-setCurrentDay();
-
-
+ /**
+  * FUNCTIONS
+  */
+ 
+ // current day is displayed at the top of the calendar
+ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+ 
+ // each time block is color-coded to indicate whether it is in the past, present, or future
+ function timeBlockColor() {
+     var hour = moment().hours();
+ 
+     $(".time-block").each(function() {
+         var currHour = parseInt($(this).attr("id"));
+ 
+         // console.log(this); //each time-block
+ 
+         if (currHour > hour) {
+             $(this).addClass("future");
+         } else if (currHour === hour) {
+             $(this).addClass("present");
+         } else {
+             $(this).addClass("past");
+         }
+     })
+ };
+ 
+ // WHEN I click the save button for that time block
+ saveBtn.on("click", function() {
+ 
+     // console.log(this); //save button
+     var time = $(this).siblings(".hour").text();
+     var plan = $(this).siblings(".plan").val();
+ 
+     // THEN the text for that event is saved in local storage
+     localStorage.setItem(time, plan);
+ });
+ 
+ // WHEN I refresh the page
+ // THEN the saved events persist
+ function usePlanner() {
+ 
+     $(".hour").each(function() {
+         var currHour = $(this).text();
+         var currPlan = localStorage.getItem(currHour);
+ 
+         // console.log(this);
+         // console.log(currHour);
+ 
+         if(currPlan !== null) {
+             $(this).siblings(".plan").val(currPlan);
+         }
+     });
+ }
+ 
+ /**
+  * CALL FUNCTIONS
+  */
+ 
+ timeBlockColor();
+ usePlanner();
